cf898729526117f1312404930665910336832aa9
Matous Vokal <matous.vokal@virtualmaster.com>
2021-11-01 12:22:44 +0100

    Componentize

---
+++ lib/drake_web/live/game_live/components.ex
@@ -0,0 +1,120 @@
+defmodule DrakeWeb.GameLive.Components do
+  alias Drake.{TroopStacks, Board, Tile, GameState, PlayingSide}
+  alias DrakeWeb.Router.Helpers, as: Routes
+
+  import Phoenix.HTML.Link
+  import DrakeWeb.ViewHelpers
+
+  use Phoenix.Component
+
+  def captured(assigns) do
+    ~H"""
+    <td class="captured"><%= "Captured: #{captured_count(@game, @side)}" %></td>
+    """
+  end
+
+  def stack(assigns) do
+    ~H"""
+    <td
+      class={if @selected, do: "selected"}
+      style={"background-image:#{stack_image(@socket, @game, @side)}"}
+      phx-click="click-stack"
+      phx-value-side={Atom.to_string(@side)}>
+    </td>
+    """
+  end
+
+  def tile(assigns) do
+    ~H"""
+    <td
+      class={class_list(selected: @selected, move: @has_move)}
+      style={"background-image:#{tile_image(@socket, @game, {@x, @y})}"}
+      phx-click="click-tile"
+      phx-value-x={@x}
+      phx-value-y={@y}>
+    </td>
+    """
+  end
+
+  def status(assigns) do
+    ~H"""
+    <section class="row">
+      <h2 class={"center #{color_class(@game)}"}><%= status_message(@game) %></h2>
+    </section>
+    <%= if GameState.victory?(@game) do %>
+      <br>
+      <section class="row">
+        <%= link "Play again", to: Routes.live_path(@socket, DrakeWeb.GameLive.Show), class: "button center" %>
+      </section>
+    <% end %>
+    """
+  end
+
+  defp captured_count(game, side) do
+    TroopStacks.length(game.board.captured_troops, side)
+  end
+
+  defp stack_image(socket, game, side) do
+    case TroopStacks.peek(game.troops, side) do
+      nil ->
+        ""
+
+      troop ->
+        path = Routes.static_path(socket, image_for_troop({troop, :front, side}))
+        "url(#{path})"
+    end
+  end
+
+  defp tile_image(socket, game, position) do
+    tile = Board.tile_at!(game.board, position)
+
+    if Tile.has_troop?(tile) do
+      troop = Tile.get_troop(tile)
+      "url(#{Routes.static_path(socket, image_for_troop(troop))})"
+    else
+      ""
+    end
+  end
+
+  defp image_for_troop({type, face, side}) do
+    type_name = side_name(type)
+
+    face_name = Atom.to_string(face)
+
+    side_initial =
+      side
+      |> side_name()
+      |> String.first()
+
+    "/images/#{face_name}#{type_name}#{side_initial}.png"
+  end
+
+  defp color_class(state) do
+    case side_for_message(state) do
+      :blue -> "blue"
+      :orange -> "orange"
+    end
+  end
+
+  defp side_name(side) do
+    side
+    |> Atom.to_string()
+    |> String.capitalize()
+  end
+
+  defp status_message(state) do
+    player =
+      state
+      |> side_for_message()
+      |> side_name()
+
+    if state.status == :victory do
+      "#{player} player won!"
+    else
+      "#{player} player's turn"
+    end
+  end
+
+  defp side_for_message(%{status: :victory} = state), do: PlayingSide.opposite(state.side_on_turn)
+  defp side_for_message(state), do: state.side_on_turn
+end
--- lib/drake_web/live/game_live/show.ex
+++ lib/drake_web/live/game_live/show.ex
@@ -1,5 +1,6 @@
 defmodule DrakeWeb.GameLive.Show do
-  alias Drake.{Board, Tile, TroopStacks, GameState, PlayingSide}
+  alias Drake.{Board, Tile, GameState}
+  import DrakeWeb.GameLive.Components
 
   use DrakeWeb, :live_view
 
@@ -50,73 +51,4 @@ defmodule DrakeWeb.GameLive.Show do
 
     {:noreply, changed_socket}
   end
-
-  defp captured_count(state, side) do
-    count = TroopStacks.length(state.board.captured_troops, side)
-    "Captured: #{count}"
-  end
-
-  defp tile_image(socket, state, position) do
-    tile = Board.tile_at!(state.board, position)
-
-    if Tile.has_troop?(tile) do
-      troop = Tile.get_troop(tile)
-      "url(#{Routes.static_path(socket, image_for_troop(troop))})"
-    else
-      ""
-    end
-  end
-
-  defp stack_image(socket, state, side) do
-    case TroopStacks.peek(state.troops, side) do
-      nil ->
-        ""
-
-      troop ->
-        path = Routes.static_path(socket, image_for_troop({troop, :front, side}))
-        "url(#{path})"
-    end
-  end
-
-  defp image_for_troop({type, face, side}) do
-    type_name = side_name(type)
-
-    face_name = Atom.to_string(face)
-
-    side_initial =
-      side
-      |> side_name()
-      |> String.first()
-
-    "/images/#{face_name}#{type_name}#{side_initial}.png"
-  end
-
-  defp color_class(state) do
-    case side_for_message(state) do
-      :blue -> "blue"
-      :orange -> "orange"
-    end
-  end
-
-  defp status_message(state) do
-    player =
-      state
-      |> side_for_message()
-      |> side_name()
-
-    if state.status == :victory do
-      "#{player} player won!"
-    else
-      "#{player} player on turn"
-    end
-  end
-
-  defp side_name(side) do
-    side
-    |> Atom.to_string()
-    |> String.capitalize()
-  end
-
-  defp side_for_message(%{status: :victory} = state), do: PlayingSide.opposite(state.side_on_turn)
-  defp side_for_message(state), do: state.side_on_turn
 end
